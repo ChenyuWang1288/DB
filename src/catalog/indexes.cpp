6eaaba8102942add6e7a54d2c848608656a7f82e
dashingss <105099350+dashingss@users.noreply.github.com>
2022-06-02 15:59:08 +0800

    Update indexes.cpp

--- src/catalog/indexes.cpp
+++ src/catalog/indexes.cpp
@@ -8,13 +8,75 @@ IndexMetadata *IndexMetadata::Create(const index_id_t index_id, const string &in
 }
 
 uint32_t IndexMetadata::SerializeTo(char *buf) const {
-  return 0;
+  char *begin = buf;
+  MACH_WRITE_UINT32(buf, INDEX_METADATA_MAGIC_NUM);
+  buf += sizeof(uint32_t);
+  MACH_WRITE_UINT32(buf, index_id_);
+  buf += sizeof(uint32_t);
+  MACH_WRITE_UINT32(buf, index_name_.length());
+  buf += sizeof(uint32_t);
+  MACH_WRITE_STRING(buf, index_name_);
+  buf += (unsigned long)index_name_.length();
+  MACH_WRITE_UINT32(buf, table_id_);
+  buf += sizeof(uint32_t);
+
+  MACH_WRITE_UINT32(buf, key_map_.size());
+  buf += sizeof(uint32_t);
+  
+  uint32_t size = key_map_.size();
+
+  for (uint32_t i = 0; i < size; i++) {
+    MACH_WRITE_UINT32(buf, key_map_[i]);
+    buf += sizeof(uint32_t);
+  }
+  uint32_t offset = buf - begin;
+  buf = begin;
+  return offset;
 }
 
 uint32_t IndexMetadata::GetSerializedSize() const {
-  return 0;
+    return sizeof(uint32_t) * (5+key_map_.size()) + (unsigned long)index_name_.length();
 }
 
 uint32_t IndexMetadata::DeserializeFrom(char *buf, IndexMetadata *&index_meta, MemHeap *heap) {
-  return 0;
+  if (index_meta != nullptr) {
+    LOG(WARNING) << "Pointer to index_meta is not null in index_meta deserialize." << std::endl;
+  }
+  char *begin = buf;
+  uint32_t magic_num = MACH_READ_UINT32(buf);
+
+  buf += sizeof(uint32_t);
+  if (magic_num != INDEX_METADATA_MAGIC_NUM) {
+    LOG(WARNING) << "MAGIC_NUM wrong in index Deserialize" << std::endl;
+    buf = begin;
+    return 0;
+  }
+  uint32_t iid = MACH_READ_UINT32(buf);
+  buf += sizeof(uint32_t);
+
+  uint32_t name_length = MACH_READ_UINT32(buf);
+  buf += sizeof(uint32_t);
+
+  char *i_name = new char[name_length + 1];
+  MACH_READ_STR(i_name, buf, name_length);
+  i_name[name_length] = 0;
+  buf += name_length;
+
+  uint32_t tid = MACH_READ_UINT32(buf);
+  buf += sizeof(uint32_t);
+
+  uint32_t len = MACH_READ_UINT32(buf);
+  buf += sizeof(uint32_t);
+
+  std::vector<uint32_t> kt;
+  for (uint32_t i = 0; i < len; i++) {
+    uint32_t a = MACH_READ_UINT32(buf);
+    kt.push_back(a);
+    buf += sizeof(uint32_t);
+  }
+  index_meta = Create(iid, i_name, tid, kt, heap);
+  size_t offset = buf - begin;
+  buf = begin;
+  delete[] i_name;
+  return offset;
 }
