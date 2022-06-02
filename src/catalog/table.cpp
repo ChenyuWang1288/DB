4f83b35de6e0aa4dde8a61ff39fc8db03f7c2d0f
dashingss <105099350+dashingss@users.noreply.github.com>
2022-06-02 15:59:33 +0800

    Update table.cpp

--- src/catalog/table.cpp
+++ src/catalog/table.cpp
@@ -1,18 +1,68 @@
 #include "catalog/table.h"
 
 uint32_t TableMetadata::SerializeTo(char *buf) const {
-  return 0;
+  char *begin = buf;
+  MACH_WRITE_UINT32(buf, TABLE_METADATA_MAGIC_NUM);
+  buf += sizeof(uint32_t);
+  MACH_WRITE_UINT32(buf, table_id_);
+  buf += sizeof(uint32_t);
+  MACH_WRITE_UINT32(buf, table_name_.length());
+  buf += sizeof(uint32_t);
+  MACH_WRITE_STRING(buf, table_name_);
+  buf += (unsigned long)table_name_.length();
+  MACH_WRITE_UINT32(buf, root_page_id_);
+  buf += sizeof(uint32_t);
+  MACH_WRITE_TO(Schema, buf, *schema_);
+  buf += sizeof(Schema);
+  uint32_t offset = buf - begin;
+  buf = begin;
+  return offset;
 }
 
 uint32_t TableMetadata::GetSerializedSize() const {
-  return 0;
+  return sizeof(uint32_t) * 4 + (unsigned long)table_name_.length() + sizeof(Schema) ;
 }
 
 /**
  * @param heap Memory heap passed by TableInfo
  */
 uint32_t TableMetadata::DeserializeFrom(char *buf, TableMetadata *&table_meta, MemHeap *heap) {
-  return 0;
+  if (table_meta != nullptr) {
+    LOG(WARNING) << "Pointer to table_meta is not null in table_meta deserialize." << std::endl;
+  }
+  char *begin = buf;
+  uint32_t magic_num = MACH_READ_UINT32(buf);
+
+  buf += sizeof(uint32_t);
+  if (magic_num != TABLE_METADATA_MAGIC_NUM) {
+    LOG(WARNING) << "MAGIC_NUM wrong in table Deserialize" << std::endl;
+    buf = begin;
+    return 0;
+  }
+  uint32_t tid = MACH_READ_UINT32(buf);
+  buf += sizeof(uint32_t);
+ 
+  uint32_t name_length = MACH_READ_UINT32(buf);
+  buf += sizeof(uint32_t);
+
+  char *t_name = new char[name_length + 1];
+  MACH_READ_STR(t_name, buf, name_length);
+  t_name[name_length] = 0;
+  buf += name_length;
+
+  uint32_t rid = MACH_READ_UINT32(buf);
+  buf += sizeof(uint32_t);
+
+  Schema *s;
+  *s=MACH_READ_FROM(Schema, buf);
+  buf += sizeof(Schema);
+
+
+  table_meta = Create(tid, t_name, rid, s, heap);
+  size_t offset = buf - begin;
+  buf = begin;
+  delete[] t_name;
+  return offset;
 }
 
 /**
