52d6e50fb47b0bb747dc48af1930f3d5ad911e4e
JSC1375 <565405659@qq.com>
2022-06-03 13:54:20 +0800

    catalogtest

--- test/catalog/catalog_test.cpp
+++ test/catalog/catalog_test.cpp
@@ -103,9 +103,7 @@ TEST(CatalogTest, CatalogIndexTest) {
     RowId rid(1000, i);
     ASSERT_EQ(DB_SUCCESS, index_info->GetIndex()->InsertEntry(row, rid, nullptr));
   }
-  // Scan Key
-  int position;
-  page_id_t leaf_page_id;
+
   std::vector<RowId> ret;
   for (int i = 0; i < 10; i++) {
     std::vector<Field> fields{
